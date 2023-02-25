dec21a13060e55e4fcbe6b6dc3effad39c7bfe0e
unknown <sumreenali111@gmail.com>
2023-02-25 17:49:43 +0500

    pipe validation implemented on create book controller and mongoose invalid id validation

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,10 +1,36 @@
+import { IsNotEmpty , IsOptional, MaxLength, MinLength, IsString, IsNumber, IsEnum } from "class-validator";
 import { Category } from "../schemas/book.schema";
 
 export class UpdateBookDto{
+    @IsOptional()
+    @MaxLength(40)
+    @MinLength(3)
+    @IsString()
+    @IsNotEmpty()
     readonly title : string;
+
+    @IsOptional()
+    @MaxLength(1000)
+    @MinLength(10)
+    @IsString()
+    @IsNotEmpty()
     readonly description : string;
+
+    @IsOptional()
+    @MaxLength(40)
+    @MinLength(3)
+    @IsString()
+    @IsNotEmpty()
     readonly author: string;
+
+    @IsOptional()
+    @IsNumber()
+    @IsNotEmpty()
     readonly price: number;
+
+    @IsOptional()
+    @IsEnum(Category, {message:'Please enter correct category'})
+    @IsNotEmpty()
     readonly category: Category;
 
 
