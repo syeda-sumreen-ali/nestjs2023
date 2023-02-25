a81000b23f78e2568d3292771b28a7ccc813e922
unknown <sumreenali111@gmail.com>
2023-02-25 17:37:28 +0500

    pipe validation implemented on create book controller

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,10 +1,35 @@
 import { Category } from "../schemas/book.schema";
+import { IsNotEmpty, IsEnum, IsNumber, IsString, MinLength, MaxLength } from "class-validator";
+
 
 export class CreateBookDto{
+
+    @MaxLength(40)
+    @MinLength(3)
+    @IsString()
+    @IsNotEmpty()
     readonly title : string;
+
+    @MaxLength(1000)
+    @MinLength(10)
+    @IsString()
+    @IsNotEmpty()
     readonly description : string;
+
+    @MinLength(3)
+    @MaxLength(40)
+    @IsString()
+    @IsNotEmpty()
     readonly author: string;
+
+    @MinLength(3)
+    @MaxLength(40)
+    @IsNumber()
+    @IsNotEmpty()
     readonly price: number;
+
+    @IsEnum(Category, {message:'Please enter correct category'})
+    @IsNotEmpty()
     readonly category: Category;
 
 
